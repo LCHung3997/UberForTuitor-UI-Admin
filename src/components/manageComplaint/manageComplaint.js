9e6922eae8ace2f798132e73f11bd759202b4420
lehung22297 <44271319+lehung22297@users.noreply.github.com>
2019-12-26 02:19:14 +0700

    update

--- src/components/manageComplaint/manageComplaint.js
+++ src/components/manageComplaint/manageComplaint.js
@@ -1,22 +1,25 @@
+/* eslint-disable radix */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable no-prototype-builtins */
-import React from "react";
-import { Table, Button, Pagination, Spinner, Form, Col } from "react-bootstrap";
-import Swal from "sweetalert2";
-import history from "../../history";
+import React from 'react';
+import {
+  Table, Button, Pagination, Spinner, Form, Col
+} from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import history from '../../history';
 
 class Complaint extends React.PureComponent {
   constructor(props) {
     super(props);
-    const { getListLimitComplaint, getListContract } = this.props;
+    const { getListLimitComplaint } = this.props;
     const page = -1;
     getListLimitComplaint(page);
     // getListContract();
   }
 
-  changePage = iPage => {
+  changePage = (iPage) => {
     // const a = document.getElementById("idState").value;
     // const { getListLimitContract, filterCotractByState } = this.props;
     // if (a === "0") {
@@ -27,13 +30,15 @@ class Complaint extends React.PureComponent {
   };
 
 
-  contractClick = id => {
-    // history.push(`/manage-detail-contract/${id}`);
+  contractClick = (id) => {
+    history.push(`/manage-detail-contract/${id}`);
   };
 
   render() {
     const { listComplaint } = this.props;
-    const { numberPages, limitComplaint, offset, page } = listComplaint;
+    const {
+      numberPages, limitComplaint, offset, page
+    } = listComplaint;
     const index = parseInt(page);
     let stt = offset;
     const arrPage = [];
@@ -41,54 +46,56 @@ class Complaint extends React.PureComponent {
       arrPage.push(i);
     }
     return (
-      <div style={{ padding: "10px" }}>
+      <div style={{ padding: '8em 2em 4em 2em' }}>
         <h1
-          style={{ textAlign: "center", marginBottom: "20px", padding: "20px" }}
+          style={{ textAlign: 'center', marginBottom: '20px', padding: '20px' }}
         >
-          <b>Danh sách các khiếu nại</b>
+          <b>Danh Sách Các Khiếu Nại</b>
         </h1>
-        {listComplaint.hasOwnProperty("limitComplaint") ? null : (<div style={{ textAlign: "center" }}>
-          <Spinner animation="grow" variant="primary" />
-          <Spinner animation="grow" variant="secondary" />
-          <Spinner animation="grow" variant="success" />
-          <Spinner animation="grow" variant="danger" />
-          <Spinner animation="grow" variant="warning" />
-          <Spinner animation="grow" variant="info" />
-          <Spinner animation="grow" variant="dark" />
-        </div>)}
+        {listComplaint.hasOwnProperty('limitComplaint') ? null : (
+          <div style={{ textAlign: 'center' }}>
+            <Spinner animation="grow" variant="primary" />
+            <Spinner animation="grow" variant="secondary" />
+            <Spinner animation="grow" variant="success" />
+            <Spinner animation="grow" variant="danger" />
+            <Spinner animation="grow" variant="warning" />
+            <Spinner animation="grow" variant="info" />
+            <Spinner animation="grow" variant="dark" />
+          </div>
+        )}
 
-        <Table striped bordered hover>
-          <thead style={{ background: "#88888A" }}>
-            <tr style={{ textAlign: "center" }}>
-              <th style={{ textAlign: "center", width: "5%" }}>STT</th>
-              <th style={{ textAlign: "center", width: "25%" }}>Học sinh</th>
-              <th style={{ textAlign: "center", width: "25%" }}>Giáo viên</th>
-              <th style={{ textAlign: "center" }}>Lí do</th>
-              <th style={{ textAlign: "center" }}>Trạng thái</th>
+        <Table striped bordered hover className="ml-2">
+          <thead style={{ background: '#FF9C43' }}>
+            <tr style={{ textAlign: 'center' }}>
+              <th style={{ textAlign: 'center', width: '5%' }}>STT</th>
+              <th style={{ textAlign: 'center', width: '25%' }}>Học sinh</th>
+              <th style={{ textAlign: 'center', width: '25%' }}>Giáo viên</th>
+              <th style={{ textAlign: 'center' }}>Lí do</th>
+              <th style={{ textAlign: 'center' }}>Trạng thái</th>
             </tr>
           </thead>
           <tbody>
-            {listComplaint.hasOwnProperty("limitComplaint")
-              ? // eslint-disable-next-line no-return-assign
-              limitComplaint.map(sk => (
-                <tr onClick={this.contractClick.bind(this, sk.id)}>
-                  <td style={{ textAlign: "center" }}>{(stt += 1)}</td>
-                  <td style={{ cursor: "pointer" }}>{sk.nameStudent}</td>
-                  <td style={{ cursor: "pointer" }}>{sk.nameTeacher}</td>
-                  <td style={{ cursor: "pointer" }}>{sk.reason}</td>
-                  <td style={{ cursor: "pointer" }}>{sk.isDone}</td>
+            {listComplaint.hasOwnProperty('limitComplaint')
+              // eslint-disable-next-line no-return-assign
+              ? limitComplaint.map((sk) => (
+                <tr onClick={this.contractClick.bind(this, sk.contractId)}>
+                  <td style={{ textAlign: 'center' }}>{(stt += 1)}</td>
+                  <td style={{ cursor: 'pointer' }}>{sk.nameStudent}</td>
+                  <td style={{ cursor: 'pointer' }}>{sk.nameTeacher}</td>
+                  <td style={{ cursor: 'pointer' }}>{sk.reason}</td>
+                  <td style={{ textAlign: 'center', color: 'red' }}>{sk.isDone ? <i className="fa fa-check" /> : <i className="fa fa-times-circle" />}</td>
                 </tr>
               ))
               : null}
           </tbody>
         </Table>
-        <div style={{ textAlign: "center" }}>
+        <div style={{ textAlign: 'center' }}>
           <Pagination>
             <Pagination.First
               disabled={numberPages === 1 || false}
               onClick={() => this.changePage(index - 1)}
             />
-            {arrPage.map(number => (
+            {arrPage.map((number) => (
               <Pagination.Item
                 active={number === index || false}
                 onClick={() => this.changePage(number)}
--- src/components/manageSkill/listSkillCompont.js
+++ src/components/manageSkill/listSkillCompont.js
@@ -1,7 +1,14 @@
-import React from "react";
-import { Table, Button, Pagination, Spinner } from "react-bootstrap";
-import Swal from "sweetalert2";
-import history from "../../history";
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable no-prototype-builtins */
+import React from 'react';
+import {
+  Table, Button, Pagination, Spinner
+} from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import history from '../../history';
 
 class ListSkill extends React.PureComponent {
   constructor(props) {
@@ -14,45 +21,45 @@ class ListSkill extends React.PureComponent {
   onnclicks = async (name, skillId) => {
     const { updateSkill } = this.props;
     const { value: ipAddress } = await Swal.fire({
-      title: "Enter your IP address",
-      input: "text",
+      title: 'Nhập kỹ năng:',
+      input: 'text',
       inputValue: name,
       showCancelButton: true,
       // eslint-disable-next-line consistent-return
-      inputValidator: value => {
+      inputValidator: (value) => {
         if (!value) {
-          return "Vui lòng nhập vào kĩ năng!";
+          return 'Vui lòng nhập vào kĩ năng!';
         }
       }
     });
 
     if (ipAddress) {
       updateSkill(skillId, ipAddress);
-      Swal.fire("Đang cập nhật");
+      Swal.fire('Đang cập nhật');
       Swal.showLoading();
       setTimeout(() => {
-        Swal.fire("Cập nhật thành công", "Kĩ năng đã được cập nhật", "success");
+        Swal.fire('Cập nhật thành công', 'Kĩ năng đã được cập nhật', 'success');
       }, 3000);
     }
   };
 
-  deleteSkill = index => {
+  deleteSkill = (index) => {
     const { deleteSingleSkill } = this.props;
     Swal.fire({
-      title: "Bạn chắc chắn muốn xóa  không?",
-      text: "Kĩ năng các giáo viên cũng sẽ bị xóa!",
-      icon: "warning",
+      title: 'Bạn chắc chắn muốn xóa  không?',
+      text: 'Kĩ năng các giáo viên cũng sẽ bị xóa!',
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Đồng Ý"
-    }).then(result => {
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Đồng Ý'
+    }).then((result) => {
       if (result.value) {
         deleteSingleSkill(index);
-        Swal.fire("Đang xóa");
+        Swal.fire('Đang xóa');
         Swal.showLoading();
         setTimeout(() => {
-          Swal.fire("Xóa thành công", "Kĩ năng đã bị xóa", "success");
+          Swal.fire('Xóa thành công', 'Kĩ năng đã bị xóa', 'success');
         }, 3000);
       }
     });
@@ -61,14 +68,14 @@ class ListSkill extends React.PureComponent {
   AddSkill = async () => {
     const { addSkill } = this.props;
     const { value: ipAddress } = await Swal.fire({
-      title: "Enter your IP address",
-      input: "text",
-      inputValue: "",
+      title: 'Nhập kỹ năng:',
+      input: 'text',
+      inputValue: '',
       showCancelButton: true,
       // eslint-disable-next-line consistent-return
-      inputValidator: value => {
+      inputValidator: (value) => {
         if (!value) {
-          return "Vui lòng nhập vào kĩ năng!";
+          return 'Vui lòng nhập vào kĩ năng!';
         }
       }
     });
@@ -78,19 +85,22 @@ class ListSkill extends React.PureComponent {
       Swal.fire(`Đang thêm kĩ năng : ${ipAddress}`);
       Swal.showLoading();
       setTimeout(() => {
-        Swal.fire("Thêm thành công", "Kĩ năng đã được thêm", "success");
+        Swal.fire('Thêm thành công', 'Kĩ năng đã được thêm', 'success');
       }, 2500);
     }
   };
 
-  changePage = iPage => {
+  changePage = (iPage) => {
     const { getLimitSkills } = this.props;
     getLimitSkills(iPage);
   };
 
   render() {
     const { listSkill } = this.props;
-    const { numberPages, limitSkill, offset, page } = listSkill;
+    const {
+      numberPages, limitSkill, offset, page
+    } = listSkill;
+    // eslint-disable-next-line radix
     const index = parseInt(page);
     let stt = offset;
     const arrPage = [];
@@ -98,67 +108,79 @@ class ListSkill extends React.PureComponent {
       arrPage.push(i);
     }
     return (
-      <div style={{ padding: "10px" }}>
+      <div style={{ padding: '8em 2em 4em 2em' }}>
         <h1
-          style={{ textAlign: "center", marginBottom: "20px", padding: "20px" }}
+          style={{ textAlign: 'center', marginBottom: '20px', padding: '20px' }}
         >
-          <b>Danh sách các kỹ năng</b>
+          <b>Danh Sách Các Kỹ Năng</b>
         </h1>
-        {listSkill.hasOwnProperty("limitSkill") ? null : (<div style={{ textAlign: "center" }}>
-          <Spinner animation="grow" variant="primary" />
-          <Spinner animation="grow" variant="secondary" />
-          <Spinner animation="grow" variant="success" />
-          <Spinner animation="grow" variant="danger" />
-          <Spinner animation="grow" variant="warning" />
-          <Spinner animation="grow" variant="info" />
-          <Spinner animation="grow" variant="dark" />
-        </div>)
-        }
-        <Button type="button" onClick={this.AddSkill}>Thêm kỹ năng</Button>
-        <Table striped bordered hover>
-          <thead style={{ background: "#88888A" }}>
-            <tr style={{ textAlign: "center" }}>
-              <th style={{ textAlign: "center", width: "10%" }}>STT</th>
-              <th style={{ textAlign: "center" }}>Họ Tên</th>
-              <th style={{ textAlign: "center", width: "10%" }}>Hành động</th>
+        {listSkill.hasOwnProperty('limitSkill') ? null : (
+          <div style={{ textAlign: 'center' }}>
+            <Spinner animation="grow" variant="primary" />
+            <Spinner animation="grow" variant="secondary" />
+            <Spinner animation="grow" variant="success" />
+            <Spinner animation="grow" variant="danger" />
+            <Spinner animation="grow" variant="warning" />
+            <Spinner animation="grow" variant="info" />
+            <Spinner animation="grow" variant="dark" />
+          </div>
+        )}
+        <div className="col-md-12 col-sm-12 float-right">
+          <Button
+            type="button"
+            onClick={this.AddSkill}
+            className="mb-3 float-right"
+            variant="success"
+          >
+            <i className="fa fa-plus-square" />
+            {' '}
+            Thêm kỹ năng
+          </Button>
+        </div>
+        <Table striped bordered hover className="ml-2">
+          <thead style={{ background: '#FF9C43' }}>
+            <tr style={{ textAlign: 'center' }}>
+              <th style={{ textAlign: 'center', width: '10%' }}>STT</th>
+              <th style={{ textAlign: 'center' }}>Tên</th>
+              <th style={{ textAlign: 'center', width: '10%' }}>Hành động</th>
             </tr>
           </thead>
           <tbody>
-            {listSkill.hasOwnProperty("limitSkill")
-              ? // eslint-disable-next-line no-return-assign
-              limitSkill.map(sk => (
-                  <tr>
-                    <td style={{ textAlign: "center" }}>{(stt += 1)}</td>
-                    <td
-                      style={{ cursor: "pointer" }}
-                      onClick={this.onnclicks.bind(this, sk.name, sk.skillId)}
-                    >
-                      {sk.name}
-                    </td>
-                    <td style={{ textAlign: "center" }}>
-                      <i
-                        onClick={this.deleteSkill.bind(this, sk.skillId)}
-                        style={{ cursor: "pointer" }}
-                        className="fa fa-trash"
-                      />
-                    </td>
-                  </tr>
-                ))
+            {listSkill.hasOwnProperty('limitSkill')
+              // eslint-disable-next-line no-return-assign
+              ? limitSkill.map((sk) => (
+                <tr>
+                  <td style={{ textAlign: 'center' }}>{(stt += 1)}</td>
+                  <td
+                    style={{ cursor: 'pointer' }}
+                    onClick={this.onnclicks.bind(this, sk.name, sk.skillId)}
+                  >
+                    {sk.name}
+                  </td>
+                  <td style={{ textAlign: 'center' }}>
+                    <i
+                      onClick={this.deleteSkill.bind(this, sk.skillId)}
+                      style={{ cursor: 'pointer' }}
+                      className="fa fa-trash"
+                    />
+                  </td>
+                </tr>
+              ))
               : null}
           </tbody>
         </Table>
-        <div style={{ textAlign: "center" }}>
+        <div style={{ textAlign: 'center' }}>
           <Pagination>
-            <Pagination.First onClick={() => this.changePage(index-1)}/>
-            {arrPage.map(number => (
-              <Pagination.Item active = {number===index || false} onClick={() => this.changePage(number)}>
+            <Pagination.First onClick={() => this.changePage(index - 1)} />
+            {arrPage.map((number) => (
+              <Pagination.Item active={number === index || false} onClick={() => this.changePage(number)}>
                 {number}
               </Pagination.Item>
             ))}
-            <Pagination.Last disabled={numberPages === 1 || numberPages === index || false} onClick={() => this.changePage(index+1)} />
+            <Pagination.Last disabled={numberPages === 1 || numberPages === index || false} onClick={() => this.changePage(index + 1)} />
           </Pagination>
         </div>
-      
+
       </div>
     );
   }
